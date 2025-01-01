bc7715e77e14eded3fc1fbf67ca499d8e73ff358
magomed <mrnavruzov@edu.hse.ru>
2025-01-01 22:29:26 +0300

    Changed internal/config/config.go, add cleanenv lib

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,41 +1,40 @@
 package config
 
 import (
-	"encoding/json"
 	"log"
 	"os"
 	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Env        string `json:"env" env-required:"true"`
-	Storage    string `json:"storage_path" env-required:"true"`
-	HTTPServer `json:"http_server"`
+	Env        string     `json:"env" env-required:"true"`
+	Storage    string     `json:"storage_path" env-required:"true"`
+	HTTPServer HTTPServer `json:"http_server"`
 }
 
 type HTTPServer struct {
-	Address     string        `json:"address" env-required:"true"`
+	Address     string            `json:"address" env-required:"true"`
 	Timeout     time.Duration `json:"timeout" env-required:"false"`
 	IdleTimeout time.Duration `json:"idle_timeout" env-required:"false"`
 }
 
-func MustLoad() *Config{
+func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
 	}
 
-	file, err := os.Open(configPath)
-	if err != nil {
-		log.Fatalf("Failed to open config file: %v", err)
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file is not exist: %s", configPath)
 	}
-	defer file.Close()
 
 	var cfg Config
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&cfg); err != nil {
-		log.Fatalf("Failed to decode config file: %v", err)
-	}
 
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+	
 	return &cfg
 }
