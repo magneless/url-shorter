cca1b69adf9e1bcd20bb7c09782089b28d786120
magomed <mrnavruzov@edu.hse.ru>
2025-01-01 22:57:05 +0300

    Realized config init and logger

--- cmd/url-shorter/main.go
+++ cmd/url-shorter/main.go
@@ -1,13 +1,50 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/magneless/url-shorter/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
-	// TODO: init config
+	cfg := config.MustLoad()
 
-	// TODO: init logger
+	log := setupLogger(cfg.Env)
 
-	// TODO: init storage: 
+	log.Info("starting url-shorter", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	// TODO: init storage:
 
 	// TODO: init router: net/http
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
